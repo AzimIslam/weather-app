8cc128e8de00ec9383b0c2c871aeeb0b19ae1154
Azim Islam <ec19205@qmul.ac.uk>
2021-03-19 20:15:24 +0000

    Added search function

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -8,10 +8,12 @@ import 'preact-material-components/Button/style.css';
 import 'preact-material-components/Theme/style.css';
 import styles from './style.css';
 import { route } from 'preact-router';
+import { useState } from 'preact/hooks';
 
 
 // In-line CSS - adds space at the top of the button 
 const searchBtnCss = { marginTop: "8px" };
+
 
 // This method is responsible for requesting the GPS location to the user
 const requestGPS = () => {
@@ -25,18 +27,30 @@ const redirectViaGPS = (pos) => {
 	route(`/weather/${pos.coords.longitude}/${pos.coords.latitude}`);
 }
 
-const Header = () => (
-	<div id={styles.homeBody}>
-		<div className={styles.container} >
-			<Typography headline3 className={styles.weatherText}>WEATHER</Typography>
-			<div className={styles.formGroup}>
-				<TextField class={styles.postcodeInput} label="Postcode" dense></TextField>
-				<Button onTap={requestGPS} onClick={requestGPS} raised style={searchBtnCss} className="mdc-theme--primary-bg">Search</Button>
+
+
+const Header = () => {
+	const [postcode, setPostcode] = useState('');
+
+	const searchViaPostcode = () => {
+		fetch(`https://api.postcodes.io/postcodes/${postcode}`)
+			.then(response => response.json())
+			.then(data => route(`/weather/${data["result"]["longitude"]}/${data["result"]["latitude"]}`))
+
+		}
+	return(
+		<div id={styles.homeBody}>
+			<div className={styles.container} >
+				<Typography headline3 className={styles.weatherText}>WEATHER</Typography>
+				<div className={styles.formGroup}>
+					<TextField onChange={(e) => setPostcode(e.target.value)} className={styles.postcodeInput} label="Postcode" dense></TextField>
+					<Button onTap={searchViaPostcode} onClick={searchViaPostcode} raised style={searchBtnCss} className="mdc-theme--primary-bg">Search</Button>
+				</div>
+				<Typography id={styles.OrText} headline3>OR</Typography>
+				<Button onTouchStart={requestGPS} onClick ={requestGPS} raised style={{minWidth: "10%", cursor: "pointer"}}>Locate me</Button>
 			</div>
-			<Typography id={styles.OrText} headline3>OR</Typography>
-			<Button onTouchStart={requestGPS} onClick ={requestGPS} raised style={{minWidth: "10%", cursor: "pointer"}}>Locate me</Button>
-		</div>
 	</div>
-);
+	);
+};
 
 export default Header;
