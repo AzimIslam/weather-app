28bd3134e6531d0ad0f91525f0bc8ddf948db8dd
Azim Islam <ec19205@qmul.ac.uk>
2021-03-21 20:29:33 +0000

    Changed weekly table

--- src/components/WeeklyTable/index.js
+++ src/components/WeeklyTable/index.js
@@ -1,9 +1,16 @@
 import styles from './style.css';
 import Typography from 'preact-material-components/Typography';
 import 'preact-material-components/Typography/style.css';
+import {h, Component} from 'preact';
+import IconButton from 'preact-material-components/IconButton';
+import 'preact-material-components/IconButton/style.css';
 
 const WeeklyTable = ({data}) => {
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const poorIcon = {};
+    const warningIcon = {};
+    const goodIcon = {content: 'check_circle'};
+    
     return <div id={styles.container}>
         {
             data.map((day, index) => {
@@ -14,8 +21,11 @@ const WeeklyTable = ({data}) => {
                             day[2].getDay() - 1 == -1 ? "Sunday": days[day[2].getDay() - 1]
                         }
                     </Typography>
+                    <IconButton className={styles.icon}>
+                        <IconButton.Icon>info</IconButton.Icon>
+                        <IconButton.Icon on >info</IconButton.Icon>
+                    </IconButton>
                     <Typography class={styles.temp}body1>{String(day[0])}&deg;</Typography>
-                    <Typography class={styles.windSpeed}body1>{String(day[1])} MPH</Typography>
                 </div>
                 );
             })
